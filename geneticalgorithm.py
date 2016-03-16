100277814ed04c6add0f540c623e27a5896760ff
DiedeKemper <diede.kemper.18@gmail.com>
2016-03-16 10:37:52 +0100

    Two fitness functions. The genetic algorithm can be applied without any errors. Test results are not yet given.

--- geneticalgorithm.py
+++ geneticalgorithm.py
@@ -5,84 +5,67 @@ Spyder Editor
 This is a temporary script file.
 """
 from numpy import random
+from fitnessFunction import computeFitnessPitchTransitions as fitness
+
 # define the length of the melody
 mlength=12
-# define the fitness function, takes a melody m and transition table p
-def fitnessfunction(m,p):
-    fitness = 1
-    for x in range (1,mlength):
-        fitness= fitness * p[m[x-1]][m[x]]
-    return fitness
-# create a random transition table  
-prob = []    
-for x in range (0,12):
-    a=random.sample(12)
-    a=a/sum(a)
-    prob.append(a)
+nrOfTones = 11
+
 # instantiate some necessary variables    
 bestscore = 0  
-melody = random.randint(0,12,mlength)
+melody = random.randint(0,nrOfTones,mlength)
 bestmelody = list(melody)
 progress = []
-mutaterange=[0,1,2,3,4,5,6,7,8,9,10,11]
+mutaterange=range(nrOfTones)
+
 #each loop decreases the number of mutations per round
 for i in range (0,5):
 #each loop mutates the currentbest, checks it's fitness and updates currentbest   
     for x in range (0,1000):
-        melody=list(bestmelody)      
+        melody=list(bestmelody)
         r=random.randint(0,mlength,abs(i-6))
         for z in r:
             mutaterangecopy=list(mutaterange)
             mutaterangecopy.remove(melody[z])
             melody[z]= random.choice(mutaterangecopy,1)[0]
-        
-        
-        currentscore = fitnessfunction(melody,prob)
+
+        currentscore = fitness(melody)
         if currentscore >= bestscore:
             bestscore = currentscore
             bestmelody = list(melody)
-        
+
         progress.append(bestscore)
-        
-            
 
 
 def mutate(notelist, mlength=12):
 
-
-    # create a random transition table  
-    prob = []    
-    for x in range (0,12):
-        a=random.sample(12)
-        a=a/sum(a)
-        prob.append(a)
-    # instantiate some necessary variables    
-    bestscore = 0  
+   # instantiate some necessary variables
+    bestscore = 0
 
     # the list we are working on comes from the midi files
     bestmelody = notelist
-    
+
     progress = []
-    mutaterange=[0,1,2,3,4,5,6,7,8,9,10,11]
+    mutaterange=range(nrOfTones)
 
     #each loop decreases the number of mutations per round
     for i in range (0,5):
 
-    #each loop mutates the currentbest, checks it's fitness and updates currentbest   
+    #each loop mutates the currentbest, checks it's fitness and updates currentbest
         for x in range (0,1000):
-            melody=list(bestmelody)      
+            melody=list(bestmelody)
             r=random.randint(0,mlength,abs(i-6))
             for z in r:
                 mutaterangecopy=list(mutaterange)
                 mutaterangecopy.remove(melody[z])
                 melody[z]= random.choice(mutaterangecopy,1)[0]
-        
-        
-            currentscore = fitnessfunction(melody,prob)
+
+
+            currentscore = fitness(melody)
             if currentscore >= bestscore:
                 bestscore = currentscore
                 bestmelody = list(melody)
-        
+
             progress.append(bestscore)
 
     return bestmelody
